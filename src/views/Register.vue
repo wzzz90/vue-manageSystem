770e2488773c9f4b3466156072004002db9a35bb
wzzz90 <152935827@qq.com>
2019-06-12 18:09:50 +0800

    修改背景

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -2,7 +2,7 @@
   <div class="register">
     <section class="form_container">
       <div class="manage_tip">
-          <span class="title">米修在线后台管理系统</span>
+          <span class="title">时倾后台</span>
           <el-form :model="registerUser" :rules="rules" ref="registerForm" label-width="80px" class="registerForm" label-position="right">
             <el-form-item label="用户名" prop="name">
               <el-input v-model="registerUser.name" auto-complete="off" placeholder="请输入用户名"></el-input>
